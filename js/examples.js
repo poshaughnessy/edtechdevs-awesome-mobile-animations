6674c12b4f48f5fd16788b3ff17f4107f48e035d
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2014-06-03 23:01:03 +0100

    Changing things around a lot based on feedback from Markus

--- js/examples.js
+++ js/examples.js
@@ -1,10 +1,32 @@
 (function() {
 
-    init();
+    reactDemo();
+    boringDemo();
 
-    function init() {
+    function reactDemo() {
 
-        //console.log('to do', document.getElementById('example1'));
+        document.getElementById('react-touch-demo').addEventListener('loadedmetadata', function() {
+            this.currentTime = 43;
+        }, false);
+
+    }
+
+    function boringDemo() {
+
+        var menuButton = document.getElementsByClassName('menu-toggle')[0],
+            menu = document.getElementsByClassName('menu')[0];
+
+        menuButton.onclick = function(event) {
+
+            if( menu.classList.contains('reveal') ) {
+                menu.classList.remove('reveal');
+            } else {
+                menu.classList.add('reveal');
+            }
+
+            event.preventDefault();
+
+        }
 
     }
 
